ee041660e0b3a1b78c73869057aa276e4bfb8af0
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2023-09-29 13:50:29 +0700

    Time: 887 ms (10.98%), Space: 30.2 MB (98.48%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if (n == 1):
+            return True
+        
+        a = nums[0]
+        b = nums[-1]
+        i = 1
+        j = n - i - 1
+        inc = True
+        dec = True
+        
+        while (i < n):
+            if (a > nums[i]):
+                inc = False
+                
+            if (b > nums[j]):
+                dec = False
+              
+            a = nums[i]
+            b = nums[j]
+            i += 1
+            j = n - i - 1
+            
+        return inc or dec
+        
