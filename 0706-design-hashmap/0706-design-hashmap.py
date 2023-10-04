762064be900693a644c4d0022a48e8c250fb4639
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2023-10-04 12:26:28 +0700

    Time: 859 ms (14.85%), Space: 43.2 MB (5.25%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -0,0 +1,20 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.hashmap = [-1 for i in range(10**6 + 1)]
+
+    def put(self, key: int, value: int) -> None:
+        self.hashmap[key] = value
+
+    def get(self, key: int) -> int:
+        return self.hashmap[key]
+
+    def remove(self, key: int) -> None:
+        self.hashmap[key] = -1
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
