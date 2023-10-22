76c7d46bf24418f28550979107c6f90ca61681ff
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2023-10-22 18:55:20 +0700

    Time: 48 ms (38.07%), Space: 16.7 MB (32.71%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,18 +1,13 @@
 from collections import defaultdict
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
         n = len(citations)
-        freq = defaultdict(int)
         
-        for i in range(1, n + 1):
-            for x in citations:
-                if x >= i:
-                    freq[i] += 1
-            
-        ans = 0
-        for i in freq:
-            if (freq[i] >= ans and freq[i] >= i):
-                ans = i
+        for i in range(n):
+            h = n - i
+            if (citations[i] >= h):
+                return h
                 
-        return ans
+        return 0
         
