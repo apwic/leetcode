4a19594e7c793d133812e01eaeef2a63d3d2daf6
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-07-09 20:01:49 +0700

    Time: 47 ms (47.89%), Space: 17.1 MB (16.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,45 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix) - 1
-        n = len(matrix[0]) - 1
-        
-        def binSearchRow(row, target):
-            l = 0
-            r = len(row) - 1
-            mid = 0
-            
+        m, n = len(matrix), len(matrix[0])
+
+        l, r = 0, m-1
+        if r > 0:
             while l <= r:
-                mid = l + (r-l)//2
-                
-                if (target == row[mid]):
+                mid = (l+r)//2
+                if matrix[mid][0] == target:
                     return True
-                
-                if (target < row[mid]):
-                    r = mid - 1
-                
-                if (target > row[mid]):
-                    l = mid + 1
-        
-        l = 0
-        r = m
-        
-        while l <= r:
-            mid = l + (r-l)//2
-            
-            if (matrix[mid][0] <= target and target <= matrix[mid][-1]):
-                return binSearchRow(matrix[mid], target)
-            
-            if (target < matrix[mid][0]):
-                r = mid - 1
-                
-            if (target > matrix[mid][-1]):
-                l = mid + 1 
-                
-        return False    
-        
-            
-            
-                
-            
-                
-        
+    
+                if matrix[mid][0] > target:
+                    r = mid-1
+                else:
+                    l = mid+1
+
+        row = l-1
+        l, r = 0, n-1
+        mid = 0
+        if r > 0:
+            while l <= r:
+                mid = (l+r)//2
+                if matrix[row][mid] == target:
+                    return True
+    
+                if matrix[row][mid] > target:
+                    r = mid-1
+                else:
+                    l = mid+1
+
+        return matrix[row][mid] == target
