fc71d2deef1f907ab49dbe6f46a528bc8442949a
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-08-02 17:22:20 +0700

    Time: 41 ms (46.69%), Space: 16.4 MB (50.46%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [0] * max(2, n+1)
+        dp[0], dp[1] = 0, 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
