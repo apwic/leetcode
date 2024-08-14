a3276d8d5c0bf411e18fda9c143c58475aca3930
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-08-14 17:35:22 +0700

    Time: 39 ms (85.41%), Space: 16.5 MB (76.84%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,23 +1,27 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
-        grid = [[-1 for j in range(n)] for i in range(m)]
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+
+        def valid(i, j):
+            return 0 <= i < m and 0 <= j < n and obstacleGrid[i][j] == 0
+
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    continue
+
+                top, left = i-1, j-1
+                valid_top, valid_left = valid(top, j), valid(i, left)
+
+                if valid_top and valid_left:
+                    dp[i][j] = dp[top][j] + dp[i][left]
+                elif valid_top:
+                    dp[i][j] = dp[top][j]
+                elif valid_left:
+                    dp[i][j] = dp[i][left]
+
+        return dp[-1][-1]
         
-        def path(i, j):
-            if (i == m or j == n):
-                return 0
-            
-            if (obstacleGrid[i][j] == 1):
-                return 0
-            
-            if (i == m-1 and j ==n-1):
-                grid[i][j] = 1
-            
-            if (grid[i][j] != -1):
-                return grid[i][j]
-            
-            grid[i][j] = path(i, j+1) + path(i+1, j)
-            return grid[i][j]
-                
-        return path(0, 0)
