b3fbe615ecdba0f5f99556749265543b783a5642
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-09-06 13:09:31 +0700

    Time: 731 ms (39.92%), Space: 54.6 MB (81.13%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        nums = set(nums)
+        prev, curr = None, head
+
+        while curr:
+            if curr.val in nums:
+                # first element removed
+                if curr == head:
+                    head = head.next
+                else:
+                    prev.next = curr.next
+            else:
+                if prev == None:
+                    prev = head
+                else:
+                    prev = prev.next
+            curr = curr.next
+        
+        return head
