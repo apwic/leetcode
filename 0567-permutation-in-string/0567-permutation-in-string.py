172fb6ca0db6cdb5ee58ae08ee6fdfce09a79309
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-10-05 21:41:14 +0700

    Time: 55 ms (74.11%), Space: 16.6 MB (62.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,20 +1,30 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        def idx(ch):
+            return ord(ch) - ord('a')
+
         n1, n2 = len(s1), len(s2)
-        freq = defaultdict(int)
 
-        for ch in s1:
-            freq[ch] += 1
+        if n1 > n2:
+            return False
 
-        def checkFreq(s, freq):
-            for ch in s:
-                if freq[ch]:
-                    freq[ch] -= 1
+        freq1, freq2 = [0] * 26, [0] * 26
 
-            return sum([freq[ch] for ch in s1]) == 0
+        for i in range(n1):
+            freq1[idx(s1[i])] += 1
+            freq2[idx(s2[i])] += 1
 
-        for i in range(n2-n1+1):
-            if checkFreq(s2[i:i+n1], freq.copy()):
+        if freq1 == freq2:
+            return True
+
+        for i in range(n1, n2):
+            start = s2[i-n1]
+            end = s2[i]
+
+            freq2[idx(start)] -= 1
+            freq2[idx(end)] += 1
+
+            if freq1 == freq2:
                 return True
 
         return False
