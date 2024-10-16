f1aef96626b3a88ffe769e16c5bdb69eb5e2638f
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-10-16 13:17:54 +0700

    Time: 38 ms (37.42%), Space: 16.7 MB (29.54%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -5,7 +5,7 @@ class Solution:
         if b > 0: heapq.heappush(pq, (-b, 'b'))
         if c > 0: heapq.heappush(pq, (-c, 'c'))
 
-        ans = ""
+        ans = []
         while pq:
             curr, char = heapq.heappop(pq)
             curr *= -1
@@ -16,17 +16,17 @@ class Solution:
 
                 next_curr, next_char = heapq.heappop(pq)
                 next_curr *= -1
-                ans += next_char
+                ans.append(next_char)
                 next_curr -= 1
 
                 if next_curr > 0:
                     heapq.heappush(pq, (-next_curr, next_char))
                 heapq.heappush(pq, (-curr, char))
             else:
-                ans += char
+                ans.append(char)
                 curr -= 1
 
                 if curr > 0:
                     heapq.heappush(pq, (-curr, char))
 
-        return ans
+        return "".join(ans)
