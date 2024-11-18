a9c2e8b464a59c1b3ca1a5745dc23d9ab6c5fe78
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-11-18 16:17:01 +0700

    Time: 0 ms (100%), Space: 16.8 MB (22.92%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.py
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.py
@@ -3,15 +3,25 @@ class Solution:
         n = len(code)
         ans = [0] * n
 
+        if k == 0:
+            return ans
+        
+        start, end = 1, k
+        window = 0
+
+        if k < 0:
+            start = n + k
+            end = n - 1
+
+        for i in range(start, end + 1):
+            window += code[i]
+
         for i in range(n):
-            curr = 0
-            for j in range(1, k+1 if k > 0 else -k+1):
-                if k < 0:
-                    j *= -1
-
-                curr += code[(i+j) % n]
-
-            ans[i] = curr
+            ans[i] = window
+            window -= code[start % n]
+            window += code[(end + 1) % n]
+            start += 1
+            end += 1
 
         return ans
                 
