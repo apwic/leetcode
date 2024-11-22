29e6ce4a4c4e4d333b482bf0e95e3cc599aab630
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-11-22 18:39:06 +0700

    Time: 51 ms (82.41%), Space: 19.3 MB (49.75%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,29 +1,10 @@
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        pattern = [[] for _ in range(m)]
+        freq_pattern = defaultdict(int)
 
-        for i in range(m):
-            curr = 1
-            for j in range(1, n):
-                if matrix[i][j] == matrix[i][j-1]:
-                    curr += 1
-                else:
-                    pattern[i].append(curr)
-                    curr = 1
+        for row in matrix:
+            pattern = "".join("T" if el == row[0] else "F" for el in row)
+            freq_pattern[pattern] += 1
 
-            pattern[i].append(curr)
-
-        ans = 0
-        for i in range(m):
-            curr = 1
-            for j in range(m):
-                if i == j:
-                    continue
-
-                if pattern[i] == pattern[j]:
-                    curr += 1
-
-            ans = max(ans, curr)
-        
-        return ans 
+        return max(freq_pattern.values())
