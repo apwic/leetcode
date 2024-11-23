0216e506a5158aa91a2cf39b8a1a7887a59ddd11
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-11-23 20:19:53 +0700

    Time: 2166 ms (20.57%), Space: 29 MB (65.69%) - LeetHub

---
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        def addToRotated(stone, row, col):
+            for i in range(col-stone, col):
+                print(i, m-row-1)
+                rotated[i][m-row-1] = '#'
+
+        m, n = len(box), len(box[0])
+        rotated = [['.' for _ in range(m)] for _ in range(n)]
+
+        for row in range(m):
+            stone = 0
+            for col in range(n):
+                if box[row][col] == '#':
+                    stone += 1
+                    continue
+
+                if box[row][col] == '*':
+                    rotated[col][m-row-1] = '*'
+                    print(col, m-row-1, rotated[col][m-row-1])
+                    addToRotated(stone, row, col)
+                    stone = 0
+
+            if stone:
+                addToRotated(stone, row, col+1)
+
+        return rotated
