6aa75122675bef512c16a0b8a37bd54773113aa5
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2024-12-31 17:04:23 +0700

    Time: 0 ms (100%), Space: 17.7 MB (28.23%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -11,16 +11,8 @@ class Solution:
                 continue
 
             cost1 = dp[day-1] + costs[0]
-            if day - 7 >= 0:
-                cost2 = dp[day-7] + costs[1]
-            else:
-                cost2 = costs[1]
-
-            if day - 30 >= 0:
-                cost3 = dp[day-30] + costs[2]
-            else:
-                cost3 = costs[2]
-
+            cost2 = dp[max(0, day-7)] + costs[1]
+            cost3 = dp[max(0, day-30)] + costs[2]
             dp[day] = min(cost1, cost2, cost3)
 
         return dp[-1]
