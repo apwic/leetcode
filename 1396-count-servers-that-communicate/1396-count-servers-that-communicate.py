cf812c776fef193ce5d7fee746a78b4ed11ea560
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2025-01-23 18:01:49 +0700

    Time: 15 ms (73.78%), Space: 19.5 MB (60.67%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
@@ -1,30 +1,20 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        visited = [[0 for _ in range(n)] for _ in range(m)]
-
+        
+        row_count = [0] * m
+        col_count = [0] * n
+        
         for i in range(m):
-            sr = None
             for j in range(n):
-                if grid[i][j] == 0:
-                    continue
-
-                if sr == None:
-                    sr = (i, j)
-                else:
-                    visited[sr[0]][sr[1]] = 1
-                    visited[i][j] = 1
-
-        for i in range(n):
-            sr = None
-            for j in range(m):
-                if grid[j][i] == 0:
-                    continue
-
-                if sr == None:
-                    sr = (j, i)
-                else:
-                    visited[sr[0]][sr[1]] = 1
-                    visited[j][i] = 1
-
-        return sum(sum(row) for row in visited)
+                if grid[i][j] == 1:
+                    row_count[i] += 1
+                    col_count[j] += 1
+        
+        result = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (row_count[i] > 1 or col_count[j] > 1):
+                    result += 1
+        
+        return result
