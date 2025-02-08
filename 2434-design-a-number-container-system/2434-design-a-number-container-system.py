7e678487de3d041f29c0bb8ae13e10ac0c0df67d
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2025-02-08 22:40:59 +0700

    Time: 107 ms (81.37%), Space: 76.4 MB (81.37%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.py
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.py
@@ -4,43 +4,23 @@ class NumberContainers:
         self.hash_idx = defaultdict(int)
 
     def change(self, index: int, number: int) -> None:
-        print("CHANGE", index, number)
-        # No number in the index, no need to replace
-        if self.hash_idx[index] == 0:
-            self.hash_idx[index] = number
-            # add to the hash_num too
-            heappush(self.hash_num[number], index)
-            return
-
-        # Replace the num
-        prev = self.hash_idx[index]
-        if prev == number:
-            return
         self.hash_idx[index] = number
-
-        # Find the corresponding index in hash_num and replace
-        heap = self.hash_num[prev]
-        indexs = []
-        while heap:
-            idx = heappop(heap)
-            if idx == index:
-                break
-            indexs.append(idx)
-
-        for idx in indexs:
-            heappush(heap, idx)
-
-        # add to the hash_num too
         heappush(self.hash_num[number], index)
 
     def find(self, number: int) -> int:
-        print("FIND", number)
         heap = self.hash_num[number]
-
-        if len(heap) == 0:
+        if not heap:
             return -1
         
-        return heap[0]
+        # Lazy remove, only when the index is found in another number
+        while heap:
+            idx = heap[0]
+            if self.hash_idx[idx] == number:
+                return idx
+
+            heappop(heap)
+
+        return -1
 
 # Your NumberContainers object will be instantiated and called as such:
 # obj = NumberContainers()
