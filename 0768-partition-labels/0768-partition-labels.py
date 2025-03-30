f3e6d00854b9c405525ee9a4660fa386dd011b9e
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2025-03-30 18:55:15 +0700

    Time: 9 ms (12.52%), Space: 17.8 MB (57.51%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -6,20 +6,19 @@ class Solution:
         for i in range(n):
             ch = s[i]
             if ch in index:
-                first, last = index[ch][0], index[ch][1]
-                index[ch] = (min(first, i), max(last, i))
+                index[ch] = max(index[ch], i)
             else:
-                index[ch] = (i, i)
+                index[ch] = i
 
         ans = []
-        min_idx, max_idx = 0, index[s[0]][1]
+        min_idx, max_idx = 0, index[s[0]]
         for i in range(n):
             if i == max_idx:
                 ans.append(max_idx - min_idx + 1)
 
                 if i+1 < n:
-                    min_idx, max_idx = i+1, index[s[i+1]][1]
+                    min_idx, max_idx = i+1, index[s[i+1]]
             else:
-                max_idx = max(max_idx, index[s[i]][1])
+                max_idx = max(max_idx, index[s[i]])
 
         return ans
